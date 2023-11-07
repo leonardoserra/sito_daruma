ceb1c65a4d29c2a4f2859edd541d953d97119cce
leonardoserra <serra.leonardo92@gmail.com>
2023-11-07 16:23:36 +0100

    styling error page, basic

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './style.scss'
+import './global.scss'
 import App from './App.vue'
 import router from './router'
 
--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,25 +1,29 @@
 <script >
   export default {
     name: 'NotFound',
-
-    data() {
-        return {
-            
-        }
-    },
-    methods: {
-        
-    },
-    props: {
-
-    },
 }
 </script>
 
 <template>
-  <h1>ERRORE PAGINA NON TROVATA</h1>
+  <div class="container">
+
+    <h1 id="title">ERRORE PAGINA NON TROVATA :( </h1>
+  </div>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+.container{
+  background-color: rgb(77, 77, 255);
+  height: 100vh;
+  display: flex;
+  justify-content:center;
+  align-items: center;
+}
+  #title{
+    padding: 20px 40px;
+    background-color: rgb(255, 255, 121);
+    border-radius: 10px;
+    color:red;
+    font-size: 3rem;
+  }
 </style>
