fe208c83ae61ae44a8128ad2e10832a7522209c8
leonardoserra <serra.leonardo92@gmail.com>
2023-11-07 15:54:19 +0100

    installed vue router

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script >
-  import HelloWorld from './components/HelloWorld.vue'
+  import { RouterLink, RouterView } from 'vue-router';
+  import HomePage from './views/HomePage.vue';
+
+  export default { 
+    components:{
+      HomePage
+    },
+    data(){
+      return{
+      }
+    }
+  }
 </script>
 
 <template>
-  
+  <HomePage></HomePage>
+  <RouterView />
 </template>
 
 <style scoped>
