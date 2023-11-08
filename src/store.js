552d5d6de865a88fa0c10bc3fb5db47c9d9e63ad
leonardoserra <serra.leonardo92@gmail.com>
2023-11-08 16:32:39 +0100

    store

--- src/store.js
+++ src/store.js
@@ -2,5 +2,6 @@ import { reactive } from "vue";
 export const store = reactive({
     artistInfo: null,
     artistId: "6zUJZaqND4ZduOsIXRH8Sb",
-    errorMessage: ""
+    errorMessage: "",
+    castoro: "ciao"
 })
